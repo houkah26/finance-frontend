47c1b2286fb0678a42092aede3699fec0b909fff
houkah26 <houkah26@gmail.com>
2017-08-01 11:37:59 -0700

    Update footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Header } from "semantic-ui-react";
 
-const Footer = () => (
-  <div className='footer'>
-    <h2>Footers!</h2>
-  </div>
-)
+const Footer = () =>
+  <div className="footer">
+    <Header size="small" textAlign="center">
+      Brought to you by{" "}
+      <a
+        href="https://github.com/houkah26"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Austin Houk
+      </a>{" "}
+      |{" "}
+      <a href="https://github.com/houkah26/finance-frontend">
+        Front-End Code{" "}
+      </a>{" "}
+      | <a href="https://github.com/houkah26/finance-backend">Back-End Code</a>
+    </Header>
+  </div>;
 
 export default Footer;
