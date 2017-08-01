eee4ffdf0560e4593eae3f20f85f37f166d93e3e
houkah26 <houkah26@gmail.com>
2017-08-01 11:46:15 -0700

    update footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Header } from "semantic-ui-react";
 
+const NewTabLink = ({ url, children }) =>
+  <a href={url} target="_blank" rel="noopener noreferrer">
+    {children}
+  </a>;
+
 const Footer = () =>
   <div className="footer">
     <Header size="small" textAlign="center">
       Brought to you by{" "}
-      <a
-        href="https://github.com/houkah26"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Austin Houk
-      </a>{" "}
-      |{" "}
-      <a href="https://github.com/houkah26/finance-frontend">
+      <NewTabLink url="https://github.com/houkah26">Austin Houk</NewTabLink> |{" "}
+      <NewTabLink url="https://github.com/houkah26/finance-frontend">
         Front-End Code{" "}
-      </a>{" "}
-      | <a href="https://github.com/houkah26/finance-backend">Back-End Code</a>
+      </NewTabLink>{" "}
+      |{" "}
+      <NewTabLink url="https://github.com/houkah26/finance-backend">
+        Back-End Code
+      </NewTabLink>
     </Header>
   </div>;
 
