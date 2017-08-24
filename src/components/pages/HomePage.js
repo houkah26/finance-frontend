265c70bf35c6379371752012df6dfc70648f49f8
houkah26 <houkah26@gmail.com>
2017-08-24 15:26:02 -0700

    update homepage content

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const HomePage = () => (
+const HomePage = () =>
   <div>
-    <h2>HomePage!</h2>
-    <p>Important reasons why you should use this stock application.</p>
-  </div>
-)
+    <h2>Welcome to React Finance</h2>
+    <p>
+      A web application where users can buy and sell stock (no real money
+      required). Please pardon the initial load time, the back-end server hosted
+      for free on Heroku has to be woken up after an extended period without
+      receiving traffic.
+    </p>
+    <p>
+      This app was built using Full-Stack JavaScript (Node, Express, MongoDB,
+      and React) as a personal project to become more familiar with the modern
+      React front-end ecosystem. See footer below for links to source code.
+    </p>
+  </div>;
 
 export default HomePage;
