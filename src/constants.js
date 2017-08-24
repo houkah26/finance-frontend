2f268e423c587f89f2ab4446d5842d67c61e6a85
houkah26 <houkah26@gmail.com>
2017-08-23 17:09:48 -0700

    make default api url non-local

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://finance-backend.herokuapp.com/api"
-    : "http://localhost:3001/api";
+// export const API_URL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://finance-backend.herokuapp.com/api"
+//     : "http://localhost:3001/api";
+
+export const API_URL = "https://finance-backend.herokuapp.com/api";
