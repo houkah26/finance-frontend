724c5c878c073e07b43bafbb213cf18121c2e89a
houkah26 <houkah26@gmail.com>
2017-08-25 16:26:01 -0700

    rename and update auth reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,41 +1,42 @@
 import {
   AUTH_USER,
   UNAUTH_USER,
-  ERROR,
+  AUTH_ERROR,
   UPDATE_USER,
   CLEAR_ERROR,
-  IS_REQUESTING
+  AUTH_REQUEST
 } from "../actions/types";
 
 const INITIAL_STATE = {
-  error: "",
-  content: "",
+  errorMessage: "",
   authenticated: false,
   user: {},
   isRequesting: false
 };
 
-export default function(state = INITIAL_STATE, action) {
+const auth = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case AUTH_USER:
       return {
         ...state,
         user: action.user,
-        error: "",
+        errorMessage: "",
         authenticated: true,
         isRequesting: false
       };
     case UNAUTH_USER:
       return { ...INITIAL_STATE };
-    case ERROR:
-      return { ...state, error: action.payload, isRequesting: false };
+    case AUTH_ERROR:
+      return { ...state, errorMessage: action.payload, isRequesting: false };
     case UPDATE_USER:
-      return { ...state, user: action.user, error: "" };
+      return { ...state, user: action.user, errorMessage: "" };
     case CLEAR_ERROR:
-      return { ...state, error: "" };
-    case IS_REQUESTING:
+      return { ...state, errorMessage: "" };
+    case AUTH_REQUEST:
       return { ...state, isRequesting: true };
     default:
       return state;
   }
-}
+};
+
+export default auth;
