4c4ae0cda5320a98cf5e1640a7d2295a94493b93
houkah26 <houkah26@gmail.com>
2017-08-29 09:58:48 -0700

    reorganize render form field component/function

--- src/components/forms/components/renderFields/index.js
+++ src/components/forms/components/renderFields/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import RenderField from "./RenderField";
 import { Field } from "redux-form";
+import FormField from "./FormField";
 
 const renderFields = inputFields =>
   inputFields.map(field =>
     <Field
       name={field.name}
       type={field.type}
-      component={RenderField}
+      component={FormField}
       key={field.name}
     />
   );
