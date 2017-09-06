ca5420114c303db4989baa0fa4ebbf0af48ae3ae
houkah26 <houkah26@gmail.com>
2017-09-06 14:44:56 -0700

    add api url and key for AV stock data

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,12 @@
 //     ? "https://finance-backend.herokuapp.com/api"
 //     : "http://localhost:3001/api";
 
+// Back-end API URL
 export const API_URL = "https://finance-backend.herokuapp.com/api";
+
+// Alpha-Vantage API URL and Key
+export const AV_API_URL = "https://www.alphavantage.co/query?";
+export const AV_API_KEY = "A6U827PR1WBNS4T1";
 
 // Responsive limit for react media queries (px)
 export const RESPONSIVE_LIMIT = 767;
