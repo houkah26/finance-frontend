44abe533f8d2561811ad09ecbcec208d2f544eb3
houkah26 <houkah26@gmail.com>
2017-09-28 13:47:51 -0700

    add accordion links to source at mobile resolutions

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import ScreenSize from "../../util/ScreenSize";
 import HomePageButtonsAtMobileRes from "./HomePageButtonsAtMobileRes";
 import HomePageAccordion from "./HomePageAccordion";
+import HomePageLinks from "./HomePageLinks";
 
 import { changeRoute } from "../../../actions/routing";
 import { RESPONSIVE_LIMIT } from "../../../constants";
@@ -17,23 +18,28 @@ const divStyle = {
 
 const HomePage = ({ authenticated, changeRoute }) => (
   <ScreenSize>
-    {screenSize => (
-      <div style={divStyle}>
-        <Header size="large">Welcome to React Finance</Header>
-        <p>
-          Stock trading simulator where authenticated users can quote, buy, and
-          stock based on real time stock data. This app was built using
-          Full-Stack JavaScript (Node, Express, MongoDB, and React) as a
-          personal project to become more familiar with the modern React
-          front-end ecosystem. See footer below for links to source code.
-        </p>
-        <HomePageButtonsAtMobileRes
-          authenticated={authenticated}
-          changeRoute={changeRoute}
-        />
-        <HomePageAccordion isMobileRes={screenSize.width < RESPONSIVE_LIMIT} />
-      </div>
-    )}
+    {screenSize => {
+      const isMobileRes = screenSize.width < RESPONSIVE_LIMIT;
+      return (
+        <div style={divStyle}>
+          <Header size="large">Welcome to React Finance</Header>
+          <p>
+            Stock trading simulator where authenticated users can quote, buy,
+            and stock based on real time stock data. This app was built using
+            Full-Stack JavaScript (Node, Express, MongoDB, and React) as a
+            personal project to become more familiar with the modern React
+            front-end ecosystem. See {isMobileRes ? "below" : "footer"} for
+            links to source code.
+          </p>
+          <HomePageButtonsAtMobileRes
+            authenticated={authenticated}
+            changeRoute={changeRoute}
+          />
+          <HomePageAccordion isMobileRes={isMobileRes} />
+          {isMobileRes && <HomePageLinks />}
+        </div>
+      );
+    }}
   </ScreenSize>
 );
 
