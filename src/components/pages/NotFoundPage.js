2ca53f040d0b3fcb50a6d7d7ab738fd7a07f9e59
houkah26 <houkah26@gmail.com>
2017-09-29 12:55:26 -0700

    add styles and update not found page

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
+
+const divStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "1rem 0"
+};
 
 const NotFoundPage = () => (
-  <div>
-    <h2>404 - Page Not Found</h2>
+  <div style={divStyle}>
+    <h2>Page Not Found</h2>
     <p>I'm sorry, the page you were looking for cannot be found!</p>
   </div>
-)
+);
 
 export default NotFoundPage;
